ca0d14d4efa36e29c5e2ff5ec7670b191d631a0f
Lovis Bero Suchmann <lovis.suchmann@rwth-aachen.de>
2026-01-20 00:19:21 +0100

    add nixpkgs-2505-250920-053548-nixpkgs-b2a3852b

---
+++ nixpkgs-2505-250920-053548-nixpkgs-b2a3852b/flake.nix
@@ -0,0 +1,7 @@
+{
+  description =
+    "nixpkgs-2505-250920-053548-nixpkgs-b2a3852b";
+  inputs.pinned.url =
+    "github:NixOS/nixpkgs/b2a3852bd078e68dd2b3dfa8c00c67af1f0a7d20";
+  outputs = { pinned, ... }: pinned;
+}
